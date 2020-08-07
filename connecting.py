9f2b00f9f86dc4ba833106433e3958934192eae2
Praditya Nugraha <53170944+Ditskih@users.noreply.github.com>
2020-08-07 22:38:36 +0700

    Add files via upload

---
+++ connecting.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 10:20:07 2020
+
+@author: Ditskih
+"""
+
+import psycopg2
+import os
+import numpy as np
+import pandas as pd
+import connection_stat as stat
+import pandas.io.sql as psql
+#import sys
+
+conn_string = "host="+ stat.postgrehost +" port="+ "5432" +" dbname="+ stat.postgredatabase +" user=" + stat.postgreuser \
++" password="+ stat.postgrepassword
+conn=psycopg2.connect(conn_string)
+print("Connected!")
+
+# Create a cursor object
+cursor = conn.cursor()
+
+
+def load_data(schema, table):
+
+    sql_command = "SELECT * FROM {}.{};".format(str(schema), str(table))
+    print (sql_command)
+
+    # Load the data
+    data = pd.read_sql(sql_command, conn)
+
+    print(data.shape)
+    return (data)
+
+jd_do = load_data("public","jd_do")
---
+++ connection_stat.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 10:14:38 2020
+
+@author: Ditskih
+"""
+
+postgrehost="localhost"
+postgredatabase="postgres"
+postgreuser="postgres"
+postgrepassword="ditskih"
