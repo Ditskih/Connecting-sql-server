7ec6f688dc14188cb1690f7c23aef7bef4c28df6
Praditya Nugraha <53170944+Ditskih@users.noreply.github.com>
2021-09-10 07:44:35 +0700

    Update connection_stat.py

--- connection_stat.py
+++ connection_stat.py
@@ -5,7 +5,7 @@ Created on Sat Jul  4 10:14:38 2020
 @author: Ditskih
 """
 
-postgrehost="localhost"
-postgredatabase="postgres"
-postgreuser="postgres"
-postgrepassword="ditskih"
+postgrehost="your_hosting_site" #if you run in your own laptop / local, you can use "localhost"
+postgredatabase="database_name"
+postgreuser="postgres_user"
+postgrepassword="postgres_pw"
